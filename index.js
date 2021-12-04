78c562b67916aaded48b8203fc23e8614eee2728
Maksim Troitski <maxim.troitsky@gmail.com>
2021-12-05 01:11:33 +0300

    no lead components issues

---
+++ index.js
@@ -0,0 +1,41 @@
+const { get } = require('./lib/https');
+const { writeFile } = require('./lib/fs');
+
+const API_URL = 'https://herocoders.atlassian.net/rest/api/3';
+
+const routes = {
+    components: '/project/IC/components',
+    search: '/search'
+}
+
+get(`${API_URL}${routes.components}`)
+    .then(components => {
+        const noLeadComponents = components.filter(({ lead }) => !lead);
+
+        const jqlComponentsNames = noLeadComponents.map(({ name }) => `component = "${name}"`);
+        const jqlIssues = `jql=project = IC AND (${jqlComponentsNames.join(' OR ')})`;
+
+        return get(`${API_URL}${routes.search}?${jqlIssues}`).then(({ issues }) => ({
+            components: noLeadComponents,
+            issues
+        }))
+    })
+    .then(({ components, issues }) => {
+        const result = components.reduce((acc, component) => {
+            acc[component.name] = issues
+                .filter(({ fields: { components: issueComponents } }) => {
+                    return issueComponents.some(({ name: icName }) => icName === component.name)
+                })
+                .map(({ id }) => id);
+
+            return acc;
+        }, {});
+
+
+        return Promise.all([
+            writeFile('./+no-lead-components.json', components),
+            writeFile('./+no-lead-components-issues.json', issues),
+            writeFile('./+final-result.json', result)
+        ])
+    })
+    .catch(console.error);
